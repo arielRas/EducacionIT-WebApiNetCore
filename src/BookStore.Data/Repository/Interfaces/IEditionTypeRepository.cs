6536d06b6d9d4cd87d7a002b1f2622b91e8f69ae
arielRas <ariel.ras.90@gmail.com>
2025-02-13 23:26:42 -0300

    Se agrega interfaz e implementación del repositorio para la entidad EditionType, incluyendo métodos para obtener, crear, actualizar y eliminar tipos de edición. Se mejora la gestión de excepciones.

--- src/BookStore.Data/Repository/Interfaces/IEditionTypeRepository.cs
+++ src/BookStore.Data/Repository/Interfaces/IEditionTypeRepository.cs
@@ -3,7 +3,12 @@ using BookStore.Data.Databases.BookStoreDb.Entities;
 
 namespace BookStore.Data.Repository.Interfaces;
 
-public interface IEditionTypeRepository : IRepository<EditionType>
+public interface IEditionTypeRepository
 {
     Task<EditionType> GetByCodeAsync(string code);
+    Task<EditionType> GetByIdAsync(int id);
+    Task<IEnumerable<EditionType>> GetAllAsync();
+    Task CreateAsync(EditionType editionType);
+    Task UpdateAsync(string code, EditionType editionType);
+    Task DeleteAsync(string code);
 }
--- src/BookStore.Data/Repository/Repositories/EditionTypeRepository.cs
+++ src/BookStore.Data/Repository/Repositories/EditionTypeRepository.cs
@@ -7,13 +7,54 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.Data.Repository.Repositories;
 
-public class EditionTypeRepository : Repository<EditionType>, IEditionTypeRepository
+public class EditionTypeRepository : IEditionTypeRepository
 {
-    public EditionTypeRepository(BookStoreDbContext context) : base(context){}
+    private readonly BookStoreDbContext _context;
+    private readonly DbSet<EditionType> _dbSet;
+    public EditionTypeRepository(BookStoreDbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<EditionType>();
+    }
 
     public async Task<EditionType> GetByCodeAsync(string code)
     {
-        return await dbSet.FirstOrDefaultAsync(g => g.Code == code)
-                          ?? throw new ResourceNotFoundException($"The resource with code: {code} has not been found");
+        return await _dbSet.FirstOrDefaultAsync(g => g.Code == code)
+            ?? throw new ResourceNotFoundException($"The edition type with code: {code} has not been found");
     }
+
+    public async Task<EditionType> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id)
+            ?? throw new ResourceNotFoundException($"The edition type with ID: {id} has not been found");
+    }
+
+    public async Task<IEnumerable<EditionType>> GetAllAsync()
+        => await _dbSet.ToListAsync();
+
+    public async Task CreateAsync(EditionType editionType)
+    {
+        await _dbSet.AddAsync(editionType);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(string code, EditionType editionType)
+    {
+        var existingEntity = await _dbSet.FirstOrDefaultAsync(e => e.Code == code)
+            ?? throw new ResourceNotFoundException($"The edition type with code: {code} has not been found");
+        
+        existingEntity.Name = editionType.Name;
+
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(string code)
+    {
+        var existingEntity = await _dbSet.FirstOrDefaultAsync(e => e.Code == code)
+            ?? throw new ResourceNotFoundException($"The edition type with code: {code} has not been found");
+
+        _dbSet.Remove(existingEntity);
+
+        await _context.SaveChangesAsync();
+    }    
 }
