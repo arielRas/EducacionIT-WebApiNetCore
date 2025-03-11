ad4c4061cd1c96bdb11c48a11049662a105bf8ae
arielRas <ariel.ras.90@gmail.com>
2025-03-10 21:10:58 -0300

    Se crea clase internal CriticalExceptionhandler, la cual loggea excepciones criticas y devuelve una excepcion personalizada con un mensaje para el controlador. Se crea la clase internal loggerExtensions, para agregar la funcionalidad al Iloger de loggear la excepcion y devolver la excepcion personaliza para el controlador.

---
+++ src/BookStore.Services/Extensions/LoggerExtensions.cs
@@ -0,0 +1,12 @@
+﻿using BookStore.Common.Exceptions;
+using BookStore.Services.Handlers;
+using Microsoft.Extensions.Logging;
+
+namespace BookStore.Services.Extensions
+{
+    internal static class LoggerExtensions
+    {
+        public static CriticalException HandleAndThrow(this ILogger logger, Exception ex, string message)
+            => CriticalExceptionHandler.Create(logger).HandleAndThrow(ex, message);
+    }
+}
---
+++ src/BookStore.Services/Handlers/CriticalExceptionHandler.cs
@@ -0,0 +1,32 @@
+using System;
+using BookStore.Common.Exceptions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace BookStore.Services.Handlers;
+
+internal class CriticalExceptionHandler
+{
+    private readonly ILogger _logger;
+    private CriticalExceptionHandler(ILogger logger)
+        => _logger = logger;
+
+    public static CriticalExceptionHandler Create(ILogger logger)
+        => new CriticalExceptionHandler(logger);
+
+    public CriticalException HandleAndThrow(Exception ex, string message)
+    {
+        var traceId = Guid.NewGuid();
+
+        _logger.LogError(ex, $"{traceId} - {message}");
+
+        CriticalException crititcalException = ex switch
+        {
+            DbUpdateException => new DataBaseException(message, traceId),
+            SecurityException => new SecurityException(message, traceId),
+            _ => throw new NotSupportedException($"Unsupported exception type: {ex.GetType().Name}")
+        };
+
+        return crititcalException;
+    }
+}
